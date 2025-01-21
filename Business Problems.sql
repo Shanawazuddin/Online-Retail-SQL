bf4238ebd6798d3e8ccee613bfc534e4fc34273d
Mohammed Shanawaz <shanawazuddin474@gmail.com>
2025-01-21 07:02:23 +0530

    Add files via upload

---
+++ Business Problems.sql
@@ -0,0 +1,15 @@
+Identify rows with missing data for critical fields (QUANTITY, UNIT_PRICE, CUSTOMER_ID).
+Update InvoiceDate to be in 'YYYY-MM-DD' format.
+Identify Total Sales per Transaction.
+Identify the Best-Selling Products.
+Identify Revenue by Product.
+Identify the Most Frequent Customers.
+Sales Analysis by Country.
+Analyze Sales Trends Over Time.
+Calculate Sales Conversion Rate.
+Identify the Most Expensive Products Sold.
+Analyze Sales by Time of Day.
+Calculate Average Sale per Invoice.
+Sales Trend by Day of Week.
+Find out how many products have never been sold.
+Find invoices with total value above a certain threshold (e.g., 1000).
